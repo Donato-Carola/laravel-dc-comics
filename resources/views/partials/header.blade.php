9301cd067f2e904c3b0bc35b966de48eea013b6c
Donato-Carola <don94@live.it>
2024-02-05 16:55:33 +0100

    inserimento header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,45 @@
 <header>
-    <h1>
-        header
-    </h1>
+    <div class="d-flex justify-content-end pe-5 " id="firstDiv">
+        <span class="pe-4">DC POWER VISA</span>
+        <span>ADDITIONAL DC SITES</span>
+    </div>
+
+   <div class="pt-3 pb-3">
+       <nav class="d-flex  align-items-center container">
+
+        <ul class="d-flex justify-content-between w-100 text-uppercase">
+            <li>
+                home
+            </li>
+            <li>
+                comics
+            </li>
+            <li>
+                movies
+            </li>
+            <li>
+                tv
+            </li>
+            <li>
+                games
+            </li>
+            <li>
+                collectibles
+            </li>
+            <li>
+                video
+            </li>
+            <li>
+               fans
+            </li>
+            <li>
+               news
+            </li>
+            <li>
+                shop
+            </li>
+        </ul>
+
+    </nav>
+</div>
 </header>
