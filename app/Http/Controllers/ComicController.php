d75f76b33f6b65f0119649b00be9ed43efa7bece
Donato-Carola <don94@live.it>
2024-02-05 16:46:36 +0100

    prima stampa in pagina

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -12,8 +13,8 @@ class ComicController extends Controller
     public function index()
     {
         //
-
-        return 'index';
+       $comics = Comic::all();
+        return view('comics.index',compact('comics'));
     }
 
     /**
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,11 +4,28 @@
 
 @section('main-content')
 
-<h1>
 
-    home
-</h1>
+<div class="container">
+    <div class="row">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3
+        row-cols-xl-4 g-3 mb-3 ">
+        @foreach ($comics as $comic)
+        <div class="col">
+         <div class="card">
+            <img src="{{$comic['thumb']}}" alt="">
+            <div class="card-body">
+                <p>
+                  {{$comic['description']}}
+                </p>
 
+            </div>
+         </div>
+        </div>
+
+        @endforeach
+    </div>
+    </div>
+</div>
 
 
 
